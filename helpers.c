af0075479f886d57fc87bcf1f93e785274499dc5
Jonas Lütolf <jonas.luetolf@outlook.com>
2024-03-13 14:47:54 +0100

    delete files in root folder

--- helpers.c
+++
@@ -1,7 +0,0 @@
-#include "helpers.h"
-#include "vector.h"
-#include <stdbool.h>
-
-bool matchsize(vector *vec1, vector *vec2){
-    return vec1->len == vec2->len;
-}
--- helpers.h
+++
@@ -1,8 +0,0 @@
-#ifndef HELPERS_H
-#define HELPERS_H
-#include "vector.h"
-#include <stdbool.h>
-bool matchsize(vector *vec1, vector *vec2);
-
-
-#endif /* HELPERS_H */
